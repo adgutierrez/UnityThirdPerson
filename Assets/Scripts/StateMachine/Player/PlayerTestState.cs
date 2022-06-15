31ebee52ed7cb6531d574283564c937408dee758
adgutierrez <adgutierrez@techbyte.net>
2022-06-14 19:01:25 -0700

    Work through course to StateMachine and Player States. Firsst test rune of StateMachine - Debug.Log

---
+++ Assets/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestState : PlayerBaseState
+{
+    public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        Debug.Log("Enter");
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        Debug.Log("Tick");
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Exit");
+    }
+        
+}
